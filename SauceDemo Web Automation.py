0fc38a1e7fdcffcd4634d35928b4d44420530853
Michael Alabi <alabimichael23@gmail.com>
2023-01-13 09:47:22 +0100

    Updated SauceDemo Web Automation

--- SauceDemo Web Automation.py
+++ SauceDemo Web Automation.py
@@ -16,16 +16,18 @@ def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://www.saucedemo.com/")
     time.sleep(4)
+
     # Enter Username and Password
     send_keys_to_element(driver.find_element(By.ID, "user-name"), "standard_user")
     time.sleep(2)
     send_keys_to_element(driver.find_element(By.ID, 'password'), "secret_sauce")
-    time.sleep(5)
+    time.sleep(3)
+
     # Log in
     log_in_button = driver.find_element(By.XPATH, '//*[@id="login-button"]')
     time.sleep(3)
     log_in_button.find_element(By.XPATH, '//*[@id="login-button"]').click()
-    time.sleep(5)
+    time.sleep(4)
     driver.close()
 
 if __name__ == '__main__':
